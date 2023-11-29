c0d7a8463781320d74870750003bc5036bafc9e9
UsamaKaleemTechmate <usamakaleem@techmatetech.com>
2023-11-29 17:41:10 +0500

    param api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req,Post,HttpCode   } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 @Controller('users')
 export class UserController {
   constructor(private readonly appService: AppService) {}
+  @Post()
+  createUser(@Req() request:Request): string {
+    return 'Create a new user';
+  }
 
   @Get()
   getAllUsers(@Req() request:Request): string {
     return 'get all users';
   }
+
 }
